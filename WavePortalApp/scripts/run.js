6f5429de8720c80f3ff84c26e216410b2298f50f
watataku11xx <watataku131122@outlook.jp>
2023-02-22 16:03:14 +0900

    first commit

---
+++ WavePortalApp/scripts/run.js
@@ -0,0 +1,29 @@
+// run.js
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  console.log("Contract added to:", waveContract.address);
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
+  /**
+   * 「👋（wave）」を送る
+   */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait(); // トランザクションが承認されるのを待つ（テスト:1回目）
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  await waveTxn.wait(); // トランザクションが承認されるのを待つ（テスト:2回目）
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
